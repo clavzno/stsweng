a2ccd898de226e11c81b3e16de764a2dcf323c82
clavzno <76804492+clavzno@users.noreply.github.com>
2025-06-15 23:39:49 +0800

    refactor/folder structure - initialized create-next-app in prep for redirect_uri

---
+++ blocki/src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import RedButton from "../components/Button";
+
+import '../pages/auth.jsx'
+
+export default function Home() {
+  return (
+    <main>
+      <RedButton />
+    </main>
+  );
+}
