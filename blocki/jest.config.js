ae0de537ffa66473917e5efc9986880a5d4d758c
Kyle Luis Silva <kyle_silva@dlsu.edu.ph>
2025-06-26 23:14:34 +0800

    test: added unit tests for pages

--- blocki/jest.config.js
+++ blocki/jest.config.js
@@ -1,10 +1,17 @@
-const nextJest = require('next/jest')
+const nextJest = require('next/jest');
 
-const createJestConfig = nextJest({ dir: './' })
+const createJestConfig = nextJest({
+  dir: './',
+});
 
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
-}
 
-module.exports = createJestConfig(customJestConfig)
+  // Add this to mock image imports (e.g. .png, .jpg, etc.)
+  moduleNameMapper: {
+    '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
--- blocki/jest.setup.js
+++ blocki/jest.setup.js
@@ -1,1 +1,28 @@
 import '@testing-library/jest-dom'
+
+// Mock window.matchMedia for tests
+if (typeof window !== 'undefined') {
+  window.matchMedia = window.matchMedia || function () {
+    return {
+      matches: false,
+      media: '',
+      onchange: null,
+      addListener: jest.fn(),    // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    };
+  };
+}
+
+// Mock IntersectionObserver for Jest
+class IntersectionObserver {
+  constructor(callback, options) {}
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+global.IntersectionObserver = IntersectionObserver;
+
