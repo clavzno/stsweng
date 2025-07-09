d3669a30f4b88d0eeaf9fbfa5b80b3f8584148b2
Jack <76804492+clavzno@users.noreply.github.com>
2025-07-09 14:36:17 +0800

    bug/removed use server from oauthcomponents.jsx

--- blocki/src/components/OauthComponents.jsx
+++ blocki/src/components/OauthComponents.jsx
@@ -1,7 +1,6 @@
 // Testing
 import { auth } from '../nextauthConfig.js';
 export default async function UserInfo() {
-  "use server";
   console.log("entered OauthComponents.jsx");
   const session = await auth();
  
