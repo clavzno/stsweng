0622c16c7f8a22325e3e130313e9200206c5c682
clavzno <76804492+clavzno@users.noreply.github.com>
2025-08-06 20:02:26 +0800

    feat/initialized some files in relation to Canvas API. pls read the descriptions, make another branch to implement these. but the first thing i need help with is replaceId in CanvasAPI.js

--- blocki/canvasService.js
+++
@@ -1,7 +0,0 @@
-import { getToken } from "@auth/core/jwt"
-import { auth } from "../auth"
-
-export default async function CanvasService(req, res) {
-    const session = await auth(req, res); // auth session
-    
-}
