b2decafe377ce01001951d51139e24a12a31b60a
clavzno <76804492+clavzno@users.noreply.github.com>
2026-03-03 18:10:59 +0800

    removed duplicate useState

--- blocki/src/components/CoursesList.jsx
+++ blocki/src/components/CoursesList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useState, useCallback } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import CourseCard from './CourseCard';
 import placeholderImage from '../assets/images/placeholder.png';
 import { Edit3, Check, Filter, Palette, Settings, X } from 'lucide-react';
